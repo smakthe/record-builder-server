3479105652d95100dda5551e4d623c175f19d638
smakthe <scmakra99@gmail.com>
2024-03-28 03:38:38 +0530

    Some updates

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-mongoose.connect('mongodb://localhost:27017/firstapi',  {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://0.0.0.0:27017/firstapi',  {useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
     console.log("Connected to database")
 })
@@ -15,8 +15,10 @@ mongoose.connect('mongodb://localhost:27017/firstapi',  {useNewUrlParser: true, 
 app.use(express.json())
 app.use('/', routes)
 app.get('/', (req, res)=>{
-    res.send("<h1>Index</h1>")
+    res.send("<h1><b>WELCOME</b></h1>")
 })
+
+
 
 app.listen(PORT,()=>{
     console.log("Application listening on Port ",PORT)
